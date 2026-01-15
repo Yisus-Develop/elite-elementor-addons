9bb67aa5a777b047ccc2aa4313ff998ab95cef9d
Yisus_dev <jesusdavid0518@hotmail.com>
2026-01-15 00:22:24 -0500

    chore: bump to 1.0.3 for auto-update test

--- includes/class-eweb-elite-core.php
+++ includes/class-eweb-elite-core.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 class EWEB_Elite_Core {
 
 	const MINIMUM_ELEMENTOR_VERSION = '3.0.0';
-	const VERSION = '1.0.2';
+	const VERSION = '1.0.3';
 
 	private static $instance = null;
 
